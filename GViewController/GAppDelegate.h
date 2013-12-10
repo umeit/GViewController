228b3547955a7cdf1fbae64013a11402c1f89238
Liu Feng <gumeit@gmail.com>
2013-12-10 12:18:26 +0800

    Initial Commit

---
+++ GViewController/GAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GAppDelegate.h
+//  GViewController
+//
+//  Created by Liu Feng on 13-12-10.
+//  Copyright (c) 2013年 Liu Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GViewController
+//
+//  Created by Liu Feng on 13-12-10.
+//  Copyright (c) 2013年 Liu Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GAppDelegate class]));
+    }
+}
